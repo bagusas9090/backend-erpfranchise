b4d4a2bff128531c03681a7b5cc27c8a991f81d3
bagusas9090 <baguscmi80@gmail.com>
2026-07-07 19:50:10 +0700

    remove swagger

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -6,21 +6,7 @@ use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
-/**
- * @OA\Info(
- *      version="1.0.0",
- *      title="Dokumentasi API ERP Franchise",
- *      description="Standar API RESTful untuk aplikasi ERP Franchise (Karyawan, SPV, Brand Owner).",
- *      @OA\Contact(
- *          email="developer@erpfranchise.local"
- *      )
- * )
- *
- * @OA\Server(
- *      url=L5_SWAGGER_CONST_HOST,
- *      description="Demo API Server"
- * )
- */
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
